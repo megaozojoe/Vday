6137a61d379a48a66179c11766c624ce538292fa
Matthew del Real <matthewadelreal@gmail.com>
2023-02-13 01:15:41 -0500

    Utilizing ConfettiJs to add to file
    I added in cute photo of me and my girlriend and confetti falls when she accepts to be my Valentine.

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-function onmouseover(){
-	document.getElementById('noButton').innerHTML
+//this function changes the file location of the image so it shows
+function show(){
+	let image = document.getElementById("image");
 	
+	image.src = "love.jpg"
 }
+//this function changes it back so it hides
+function hide(){
+	let image = document.getElementById("image");
 	
+	image.src = ""
+}
